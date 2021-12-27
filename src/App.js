c960ff6ce071d46a4a151aa95d6d41f6ae8e80d9
Your Name <you@example.com>
2021-12-27 15:28:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import Home from './Pages/Home/Home';
 import './App.css';
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/users">
+         
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+      </Router>
+
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import   '../Home/Home.css';
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2> THis noob Home</h2>
+        </div>
+    );
+};
+
+export default Home;
